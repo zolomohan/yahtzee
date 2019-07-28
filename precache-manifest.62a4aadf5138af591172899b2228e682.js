a159eff3656154108b04a30d6e218c0ec9a2e8df
ZoloMohan <mohanzolomon@gmail.com>
2019-07-28 18:44:47 +0530

    Updates

---
+++ precache-manifest.62a4aadf5138af591172899b2228e682.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5538c16531713b63095cc5aabefea7f2",
+    "url": "/yahtzee/index.html"
+  },
+  {
+    "revision": "de0a2657cafb78882628",
+    "url": "/yahtzee/static/css/2.d9237602.chunk.css"
+  },
+  {
+    "revision": "db8d6d9f12738c1e221c",
+    "url": "/yahtzee/static/css/main.0147c88d.chunk.css"
+  },
+  {
+    "revision": "de0a2657cafb78882628",
+    "url": "/yahtzee/static/js/2.09a2daba.chunk.js"
+  },
+  {
+    "revision": "db8d6d9f12738c1e221c",
+    "url": "/yahtzee/static/js/main.a74f162e.chunk.js"
+  },
+  {
+    "revision": "7218b0ef6013de33045e",
+    "url": "/yahtzee/static/js/runtime~main.0be27dd1.js"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/yahtzee/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/yahtzee/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/yahtzee/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/yahtzee/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/yahtzee/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/yahtzee/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/yahtzee/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/yahtzee/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/yahtzee/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/yahtzee/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/yahtzee/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/yahtzee/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/yahtzee/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/yahtzee/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/yahtzee/static/media/fa-solid-900.ef3df984.eot"
+  }
+]);
