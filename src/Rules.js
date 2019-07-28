f966a12500dcc9f70794b125b7453a520540fc10
ZoloMohan <mohanzolomon@gmail.com>
2019-07-28 17:19:23 +0530

    Added Rules Description

--- src/Rules.js
+++ src/Rules.js
@@ -97,29 +97,29 @@ class Yahtzee extends Rule {
 }
 
 // ones, twos, etc score as sum of that value
-const ones = new TotalOneNumber({ val: 1 });
-const twos = new TotalOneNumber({ val: 2 });
-const threes = new TotalOneNumber({ val: 3 });
-const fours = new TotalOneNumber({ val: 4 });
-const fives = new TotalOneNumber({ val: 5 });
-const sixes = new TotalOneNumber({ val: 6 });
+const ones = new TotalOneNumber({ val: 1, description: '1 Point Per 1' });
+const twos = new TotalOneNumber({ val: 2, description: '2 Point Per 2' });
+const threes = new TotalOneNumber({ val: 3, description: '3 Point Per 3' });
+const fours = new TotalOneNumber({ val: 4, description: '4 Point Per 4' });
+const fives = new TotalOneNumber({ val: 5, description: '5 Point Per 5' });
+const sixes = new TotalOneNumber({ val: 6, description: '6 Point Per 6' });
 
 // three/four of kind score as sum of all dice
-const threeOfKind = new SumDistro({ count: 3 });
-const fourOfKind = new SumDistro({ count: 4 });
+const threeOfKind = new SumDistro({ count: 3, description: 'Sum all dice if 3 are same' });
+const fourOfKind = new SumDistro({ count: 4, description: 'Sum all dice if 4 are same' });
 
 // full house scores as flat 25
-const fullHouse = new FullHouse({score: 25});
+const fullHouse = new FullHouse({score: 25, description: '25 Points for fullhouse'});
 
 // small/large straights score as 30/40
-const smallStraight = new SmallStraight({score: 30});
-const largeStraight = new LargeStraight({ score: 40 });
+const smallStraight = new SmallStraight({score: 30, description: '30 Points for a small straight'});
+const largeStraight = new LargeStraight({ score: 40, description: '40 Points for a large straight' });
 
 // yahtzee scores as 50
-const yahtzee = new Yahtzee({ score: 50 });
+const yahtzee = new Yahtzee({ score: 50, description: '50 Points if all dice are same' });
 
 // for chance, can view as some of all dice, requiring at least 0 of a kind
-const chance = new SumDistro({ count: 0 });
+const chance = new SumDistro({ count: 0, description: 'Sum of all dice' });
 
 export {
   ones,
