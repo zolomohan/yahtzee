9ebb804b23b91cc16ed52237323672b37ae06b2a
ZoloMohan <mohanzolomon@gmail.com>
2019-07-28 17:50:15 +0530

    Added Dice and Game.css Updtae

--- src/Die.js
+++ src/Die.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import "./Die.css";
+import '@fortawesome/fontawesome-free/css/all.css'
 
 class Die extends Component {
+
+  static defaultProps = {
+    numberNames : ['one', 'two', 'three', 'four', 'five', 'six']
+  }
+
   render() {
+    const {locked, val, idx, numberNames, handleClick, disabled} = this.props;
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={ () => {this.props.handleClick(this.props.idx)}}
+      <i
+        className={`Die fa-5x fas fa-dice-${numberNames[val - 1]} ${locked && 'Die-locked'}`}
+        onClick={ () => {handleClick(idx)}}
+        disabled = {disabled}
       >
-        {this.props.val}
-      </button>
+      </i>
     );
   }
 }
